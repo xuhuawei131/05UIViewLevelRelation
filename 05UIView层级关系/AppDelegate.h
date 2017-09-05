eb46836fc55a7a9e373e222adeae8f6befb6febc
lingdian <lingdian@meili-inc.com>
2017-09-05 15:31:06 +0800

    Initial Commit

---
+++ 05UIView层级关系/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  05UIView层级关系
+//
+//  Created by lingdian on 17/9/5.
+//  Copyright © 2017年 lingdian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 05UIView层级关系/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  05UIView层级关系
+//
+//  Created by lingdian on 17/9/5.
+//  Copyright © 2017年 lingdian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
